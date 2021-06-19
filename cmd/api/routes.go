4e2c1fda97b34bc81a739082db96594d056e07a0
omirzakye <omirzak.ye@gmail.com>
2021-06-19 23:34:22 +0500

    chapter 13

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -18,5 +18,6 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/v1/books/:id", app.updateBookHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/books/:id", app.deleteBookHandler)
 
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	return app.recoverPanic(app.rateLimit(router))
 }
