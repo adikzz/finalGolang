9f6f3274307324c608e98dbff63e4e72fe3dedfa
omirzakye <omirzak.ye@gmail.com>
2021-06-20 04:49:45 +0500

    chapter 18

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/adikzz/finalGolang/internal/mailer"
 	_ "github.com/lib/pq"
 	"os"
+	"strings"
 	"sync"
 	"time"
 )
@@ -35,6 +36,10 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -67,6 +72,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "4459d311c38754", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "66a584466bf233", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.alexedwards.net>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
